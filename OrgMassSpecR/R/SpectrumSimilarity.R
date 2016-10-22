e60a4c156e89a280746828acd94bc05cafed1c8c
Nathan Dodder <OrgMassSpec@users.noreply.github.com>
2016-10-21 19:43:56 -0700

    Added printing option arguments to SpectrumSimilarity.

--- OrgMassSpecR/R/SpectrumSimilarity.R
+++ OrgMassSpecR/R/SpectrumSimilarity.R
@@ -1,41 +1,46 @@
 SpectrumSimilarity <- function(spec.top, spec.bottom, t = 0.25, b = 10, top.label = NULL, 
-                               bottom.label = NULL, xlim = c(50, 1200)) {
-                           	
-
-    ## format spectra and normalize intensitites
-
-    top_tmp <- data.frame(mz = spec.top[,1], intensity = spec.top[,2])
-    top_tmp$normalized <- round((top_tmp$intensity / max(top_tmp$intensity)) * 100)
-    top_tmp <- subset(top_tmp, top_tmp$mz >= xlim[1] & top_tmp$mz <= xlim[2])   
-    top_plot <- data.frame(mz = top_tmp$mz, intensity = top_tmp$normalized)   # data frame for plotting spectrum
-    top <- subset(top_plot, top_plot$intensity >= b)   # data frame for similarity score calculation
-
-    bottom_tmp <- data.frame(mz = spec.bottom[,1], intensity = spec.bottom[,2])
-    bottom_tmp$normalized <- round((bottom_tmp$intensity / max(bottom_tmp$intensity)) * 100)
-    bottom_tmp <- subset(bottom_tmp, bottom_tmp$mz >= xlim[1] & bottom_tmp$mz <= xlim[2])   
-    bottom_plot <- data.frame(mz = bottom_tmp$mz, intensity = bottom_tmp$normalized)   # data frame for plotting spectrum
-    bottom <- subset(bottom_plot, bottom_plot$intensity >= b)   # data frame for similarity score calculation
-    
-    
-    ## align the m/z axis of the two spectra, the bottom spectrum is used as the reference
-
-    for(i in 1:nrow(bottom))
-        top[,1][bottom[,1][i] >= top[,1] - t & bottom[,1][i] <= top[,1] + t] <- bottom[,1][i]
-    alignment <- merge(top, bottom, by = 1, all = TRUE)
-    if(length(unique(alignment[,1])) != length(alignment[,1])) warning("the m/z tolerance is set too high")
-    alignment[,c(2,3)][is.na(alignment[,c(2,3)])] <- 0   # convert NAs to zero (R-Help, Sept. 15, 2004, John Fox)
-    names(alignment) <- c("mz", "intensity.top", "intensity.bottom")
+                               bottom.label = NULL, xlim = c(50, 1200), print.alignment = FALSE,
+                               print.graphic = TRUE) {
+  
+  
+  ## format spectra and normalize intensitites
+  
+  top_tmp <- data.frame(mz = spec.top[,1], intensity = spec.top[,2])
+  top_tmp$normalized <- round((top_tmp$intensity / max(top_tmp$intensity)) * 100)
+  top_tmp <- subset(top_tmp, top_tmp$mz >= xlim[1] & top_tmp$mz <= xlim[2])   
+  top_plot <- data.frame(mz = top_tmp$mz, intensity = top_tmp$normalized)   # data frame for plotting spectrum
+  top <- subset(top_plot, top_plot$intensity >= b)   # data frame for similarity score calculation
+  
+  bottom_tmp <- data.frame(mz = spec.bottom[,1], intensity = spec.bottom[,2])
+  bottom_tmp$normalized <- round((bottom_tmp$intensity / max(bottom_tmp$intensity)) * 100)
+  bottom_tmp <- subset(bottom_tmp, bottom_tmp$mz >= xlim[1] & bottom_tmp$mz <= xlim[2])   
+  bottom_plot <- data.frame(mz = bottom_tmp$mz, intensity = bottom_tmp$normalized)   # data frame for plotting spectrum
+  bottom <- subset(bottom_plot, bottom_plot$intensity >= b)   # data frame for similarity score calculation
+  
+  
+  ## align the m/z axis of the two spectra, the bottom spectrum is used as the reference
+  
+  for(i in 1:nrow(bottom))
+    top[,1][bottom[,1][i] >= top[,1] - t & bottom[,1][i] <= top[,1] + t] <- bottom[,1][i]
+  alignment <- merge(top, bottom, by = 1, all = TRUE)
+  if(length(unique(alignment[,1])) != length(alignment[,1])) warning("the m/z tolerance is set too high")
+  alignment[,c(2,3)][is.na(alignment[,c(2,3)])] <- 0   # convert NAs to zero (R-Help, Sept. 15, 2004, John Fox)
+  names(alignment) <- c("mz", "intensity.top", "intensity.bottom")
+  
+  if(print.alignment == TRUE) {
     print(alignment)
-
-
-    ## similarity score calculation
-
-    u <- alignment[,2]; v <- alignment[,3]
-    similarity_score <- as.vector((u %*% v) / (sqrt(sum(u^2)) * sqrt(sum(v^2))))
-    
-    
-    ## generate plot
-
+  }
+  
+  
+  ## similarity score calculation
+  
+  u <- alignment[,2]; v <- alignment[,3]
+  similarity_score <- as.vector((u %*% v) / (sqrt(sum(u^2)) * sqrt(sum(v^2))))
+  
+  
+  ## generate plot
+  
+  if(print.graphic == TRUE) {
     plot.new()
     plot.window(xlim = xlim, ylim = c(-125, 125))
     ticks <- c(-100, -50, 0, 50, 100)
@@ -50,9 +55,10 @@ SpectrumSimilarity <- function(spec.top, spec.bottom, t = 0.25, b = 10, top.labe
     plot.window(xlim = c(0, 20), ylim = c(-10, 10))
     text(10, 9, top.label)
     text(10, -9, bottom.label)
-
-    return(similarity_score)
-
-    # simscore <- as.vector((u %*% v)^2 / (sum(u^2) * sum(v^2)))   # cos squared
-
+  }
+  
+  return(similarity_score)
+  
+  # simscore <- as.vector((u %*% v)^2 / (sum(u^2) * sum(v^2)))   # cos squared
+  
 }
